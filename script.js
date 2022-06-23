e17687a21eda8e8a52b0930ba07ccc5c650d3238
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-06-23 13:26:06 -0400

    "changes"

--- script.js
+++ script.js
@@ -6,18 +6,22 @@
 // .then(data => {
 //   console.log(data);
 // })
-function fetchData(){
-  fetch("https://www.watchbase.com")
-  .then(res=>res.json())
-  .then(data =>console.log(data))
-}
+body: JSON.stringify()
+function fetchData(data){
+  const url = ("http://universities.hipolabs.com/search?country=United+Kingdom")
+  fetch (url)
+  
+  .then (res => res.json())
+  .then (data => console.log(data))
+};
 
 // element.addEventListener("click",fetchData);
 const doc= document.getElementById('form-submit')
-
+const f ="fetchwatch"
 function fetchWatch() {
-  alert ("watch");
+  alert ("university");
 }
 // const even = document.querySelector('data')
 // console.log(even)
 addEventListener("click", fetchData)
+ 
