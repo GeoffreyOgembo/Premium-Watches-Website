7a8bcab39fe1fdd6238e9db2aa8ebbbf4753d746
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-06-25 16:05:40 -0400

    first commit

--- script.js
+++ script.js
@@ -6,22 +6,35 @@
 // .then(data => {
 //   console.log(data);
 // })
-body: JSON.stringify()
-function fetchData(data){
-  const url = ("http://universities.hipolabs.com/search?country=United+Kingdom")
-  fetch (url)
-  
-  .then (res => res.json())
-  .then (data => console.log(data))
-};
+// body: JSON.stringify()
+const input = document.getElementById("inputValue")
+
+
 
 // element.addEventListener("click",fetchData);
-const doc= document.getElementById('form-submit')
-const f ="fetchwatch"
-function fetchWatch() {
-  alert ("university");
-}
+const form= document.getElementById('form-submit')
+
+function fetchWatch(event) {
+  event.preventDefault();
+  
+  const search = input.value;
+
+  console.log(search)
+    // fetch(`http://universities.hipolabs.com/search?country=${search}`)
+    fetch(`http://universities.hipolabs.com/search?name=${search}`)
+    .then(res => res.json())
+    .then(data => {
+    //   console.log(data)
+    //   var universities = Object.values(data)
+    //   universities.map((university =>{
+    //     console.log(university[1].name)
+      console.log(data[0].domains)
+    //   })
+    //   )
+    })
+    
+    }
+
 // const even = document.querySelector('data')
 // console.log(even)
-addEventListener("click", fetchData)
- 
+form.addEventListener('submit', fetchWatch)
